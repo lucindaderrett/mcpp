40e683217b5415a5fad7c75498e927246d84d4e6
lucindaderrett <s3968010@student.rmit.edu.au>
2023-10-08 18:30:01 +1100

    dont know what these mean

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lucindaderrett/Desktop/PS2/mcpp
+# Build directory: /Users/lucindaderrett/Desktop/PS2/mcpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lucindaderrett/Desktop/PS2/mcpp/test
+# Build directory: /Users/lucindaderrett/Desktop/PS2/mcpp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
